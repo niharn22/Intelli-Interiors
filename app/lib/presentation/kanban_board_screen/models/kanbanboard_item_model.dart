cab602fbfccd0f51e9c430e7cf18717702862a05
Dhruv Karia <dkaria1207@gmail.com>
2024-01-31 22:21:09 +0530

    Delete app directory

--- app/lib/presentation/kanban_board_screen/models/kanbanboard_item_model.dart
+++
@@ -1,8 +0,0 @@
-class KanbanboardItemModel {
-  String brainstormingTxt = "Brainstorming";
-
-  String languageTxt =
-      "Brainstorming brings team members' diverse experience into play. ";
-
-  String? id = "";
-}
